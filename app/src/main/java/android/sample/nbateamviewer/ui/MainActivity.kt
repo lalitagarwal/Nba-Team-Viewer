e89ce03d582c9971ea9a99f50a672fff2ce8f835
Lalit <agarwal.lalit91@gmail.com>
2019-03-12 21:41:53 -0400

    Add MainActivity

---
+++ app/src/main/java/android/sample/nbateamviewer/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package android.sample.nbateamviewer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.sample.nbateamviewer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
